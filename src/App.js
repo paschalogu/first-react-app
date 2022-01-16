20cd8f305928782605258640fc05b59d4dcba7d5
Paschal Ikenna <ikbros16@yahoo.com>
2022-01-16 20:09:58 +0100

    first react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1> My first react App</h1>
+
+      <lable htmlFor="name"> Enter Name: </lable>
+      <input id="name" type="text" />
+      <button style={{ background: "purple", color: "white" }}> Submit </button>
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +23,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
